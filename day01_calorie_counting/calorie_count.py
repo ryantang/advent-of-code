d65496dbf40ae712b72b7160ac239b988637303b
Ryan Tang <ryantang@gmail.com>
2022-12-04 14:18:05 -0800

    Refactored Day 1 Calorie Count
    Updated constructor to take a calorie_list as its primary argument.
    Created a class method to instantiate a calorie counter from a file.
    Updated max_calories method to use its own storied calorie_list.

--- day01_calorie_counting/calorie_count.py
+++ day01_calorie_counting/calorie_count.py
@@ -1,17 +1,22 @@
-class CalorieCount:
-    def most_calories(file):
-        f = open(file, 'r')
-        all_lines = f.readlines()
+class CalorieCounter:
+
+    def __init__(self, calorie_list):
+        self.calorie_list = calorie_list
+
+    @classmethod
+    def from_file(cls, calorie_file):
+        f = open(calorie_file, 'r')
+        calorie_list = f.readlines()
         f.close()
-        return CalorieCount.max_calories(all_lines)
+        return cls(calorie_list)
 
 
-    def max_calories(calorie_list):
+    def max_calories(self):
         max_calories = 0
         elf_number = 1
         top_elf = [0, 0]
         
-        for item_calories in calorie_list:
+        for item_calories in self.calorie_list:
             if item_calories == "\n":
                 elf_number = elf_number + 1
                 max_calories = 0
--- day01_calorie_counting/test_calorie_count.py
+++ day01_calorie_counting/test_calorie_count.py
@@ -3,17 +3,18 @@ import unittest
 from calorie_count import *
 
 class TestingCalorieCount(unittest.TestCase):
-
-    def test_most_calories(self):
-        self.assertEqual(CalorieCount.most_calories("/Users/ryantang/workspace/advent-of-code/day01_calorie_counting/calorie_log.txt"), [4, 24000])
-
     def test_max_calories(self):
         calorie_list = ['1\n', '2\n', '3\n'] # one elf with 6 total calories
-        self.assertEqual(CalorieCount.max_calories(calorie_list), [1,6])
+        calorie_counter = CalorieCounter(calorie_list)
+        self.assertEqual(calorie_counter.max_calories(), [1,6])
 
         calorie_list = ['1\n', '2\n', '3\n', '\n', '6\n', '7\n'] # two elves: first with 6 calories and second with 13 calories
-        self.assertEqual(CalorieCount.max_calories(calorie_list), [2,13])
+        calorie_counter = CalorieCounter(calorie_list)
+        self.assertEqual(calorie_counter.max_calories(), [2,13])
 
+    def test_with_file(self):
+        calorie_counter = CalorieCounter.from_file("/Users/ryantang/workspace/advent-of-code/day01_calorie_counting/calorie_log.txt")
+        self.assertEqual(calorie_counter.max_calories(), [4, 24000])
 
 if __name__ == '__main__':
     unittest.main()
