d2b2fd8e337f249b686114c84d31d4fef2e1b9e3
Ryan Tang <ryantang@gmail.com>
2022-12-19 17:17:04 -0800

    Day 03 Part 2. Rucksack badge problem.
    Things that I would do if this were production code:
    
    1) Memoize `scores` hash in prioirty_scores. I'm generating it everytime from scratch here.
    2) Make priority_scores a private method.

--- day03_rucksack_reorganization/rucksack.py
+++ day03_rucksack_reorganization/rucksack.py
@@ -12,21 +12,51 @@ class Rucksack:
         return compartment1.intersection(compartment2)
 
     def total_priority(rucksacks):
-        priority_scores = {}
-        for i in range(52):
-            priority_scores.update({string.ascii_letters[i]: i+1})
-
         total_score = 0
         for contents in rucksacks:
             shared_item = next(iter(Rucksack.shared_items(contents)))
-            total_score = total_score + priority_scores[shared_item]
+            total_score = total_score + Rucksack.priority_scores(shared_item)
 
         return total_score
 
 
     def total_priority_from_file(inventory_file):
         f = open(inventory_file, 'r')
-        rucksacks = f.readlines()
+        rucksacks = f.read().splitlines()
         f.close()
 
         return Rucksack.total_priority(rucksacks)
+
+    def find_badge(three_rucksacks):
+        elf1 = set(three_rucksacks[0])
+        elf2 = set(three_rucksacks[1])
+        elf3 = set(three_rucksacks[2])
+
+        return set.intersection(elf1, elf2, elf3)
+
+
+    def badge_total_priority(rucksacks):
+        if len(rucksacks) % 3 != 0:
+            raise Exception("Danger! Elves must go in groups of three.")
+
+        total_priority = 0
+        for i in range(int(len(rucksacks)/3)):
+            three_rucksacks = rucksacks[i*3:i*3+3]
+            badge = next(iter(Rucksack.find_badge(three_rucksacks)))
+            total_priority = total_priority + Rucksack.priority_scores(badge)
+        
+        return total_priority
+
+    def badge_priority_from_file(inventory_file):
+        f = open(inventory_file, 'r')
+        rucksacks = f.read().splitlines()
+        f.close()
+
+        return Rucksack.badge_total_priority(rucksacks)
+
+    def priority_scores(item):
+        scores = {}
+        for i in range(52):
+            scores.update({string.ascii_letters[i]: i+1})
+
+        return scores[item]
--- day03_rucksack_reorganization/test_rucksack.py
+++ day03_rucksack_reorganization/test_rucksack.py
@@ -24,5 +24,53 @@ class TestingRucksack(unittest.TestCase):
 
         self.assertEqual(Rucksack.total_priority_from_file(rucksacks_file), 157)
 
+
+    def test_find_badge(self):
+        rucksacks = [
+            'vJrwpWtwJgWrhcsFMMfFFhFp',
+            'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+            'PmmdzqPrVvPwwTWBwg'
+        ]
+
+        self.assertEqual(Rucksack.find_badge(rucksacks), {'r'})
+
+        rucksacks = [
+            'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+            'ttgJtRGJQctTZtZT',
+            'CrZsJsPPZsGzwwsLwLmpwMDw'
+        ]
+
+        self.assertEqual(Rucksack.find_badge(rucksacks), {'Z'})
+
+    def test_badge_priorities(self):
+        rucksacks = [
+            'vJrwpWtwJgWrhcsFMMfFFhFp',
+            'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+            'PmmdzqPrVvPwwTWBwg',
+            'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+            'ttgJtRGJQctTZtZT',
+            'CrZsJsPPZsGzwwsLwLmpwMDw'
+        ]
+
+        self.assertEqual(Rucksack.badge_total_priority(rucksacks), 18 + 52)
+
+    def test_badge_priorities_error(self):
+        wrong_number_of_rucksacks = [       #must be multiples of threes
+            'vJrwpWtwJgWrhcsFMMfFFhFp',
+            'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+            'PmmdzqPrVvPwwTWBwg',
+            'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+            'ttgJtRGJQctTZtZT'
+        ]
+
+        with self.assertRaises(Exception):
+            Rucksack.badge_total_priority(wrong_number_of_rucksacks)
+
+    
+    def test_badge_priority_from_file(self):
+        rucksacks_file = "/Users/ryantang/workspace/advent-of-code/day03_rucksack_reorganization/rucksack_inventory.txt"
+
+        self.assertEqual(Rucksack.badge_priority_from_file(rucksacks_file), 70)
+
 if __name__ == '__main__':
     unittest.main()
