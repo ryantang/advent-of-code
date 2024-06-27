9a72d921d459c8d892307008b2a9853ecaa4b010
Ryan Tang <ryan.tang@Ryans-MacBook-Pro-2.local>
2024-06-27 07:15:09 -0700

    Complete part 2 of day 1
    This required converting both spellings of numbers as well as digits.
    For example "one" and "1" both needed to be selected. The hard part
    was that "twone" needed to become "21" with overlapping letters.

--- day01_trebuchet/test_trebuchet.py
+++ day01_trebuchet/test_trebuchet.py
@@ -13,7 +13,38 @@ class TestingDecoder(unittest.TestCase):
         text_line = "a1b2c3d4e5f" # first digit is 3, second digit is 8
         self.assertEqual(Decoder.decode(text_line), 15)
 
-        text_line = "treb7uchet" # first digit is 3, second digit is 8
+        text_line = "treb7uchet" 
+        self.assertEqual(Decoder.decode(text_line), 77)
+
+    def test_decode_with_spelling(self):
+        text_line = "two1nine" #first digit is 2, second digit is 9
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 29)
+
+        text_line = "eightwothree"
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 83)
+
+        text_line = "abcone2threexyz"
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 13)
+ 
+        text_line = "xtwone3four"
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 24)
+ 
+        text_line = "4nineeightseven2" # first digit is 4, second digit is 2
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 42)
+
+        text_line = "zoneight234"
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 14)
+ 
+        text_line = "7pqrstsixteen"
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 76)
+
+        text_line = "twone"
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 21)
+
+        text_line = "eightfour2fourvzksqhxmlkpkfktmdzpmthreetwonehv" #need to convert the last "one"
+        self.assertEqual(Decoder.decode_with_spelling(text_line), 81)
+
+        text_line = "treb7uchet" 
         self.assertEqual(Decoder.decode(text_line), 77)
 
     def test_with_file(self):
@@ -22,6 +53,14 @@ class TestingDecoder(unittest.TestCase):
 
         file = "/Users/ryan.tang/workspace/advent-of-code/day01_trebuchet/input"
         self.assertEqual(Decoder.sum_decoded_from_file(file), 54597)
+
+    def test_with_file_and_spelling(self):
+        file = "/Users/ryan.tang/workspace/advent-of-code/day01_trebuchet/sample2.txt"
+        self.assertEqual(Decoder.sum_decoded_with_spelling_from_file(file), 281)
+
+        file = "/Users/ryan.tang/workspace/advent-of-code/day01_trebuchet/input"
+        self.assertEqual(Decoder.sum_decoded_with_spelling_from_file(file), 54504)
+
 
 if __name__ == '__main__':
     unittest.main()
--- day01_trebuchet/trebuchet.py
+++ day01_trebuchet/trebuchet.py
@@ -1,21 +1,51 @@
+import re
+
+translator = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
 class Decoder:
     def decode(text_line):
-        decoded = Decoder.first_digit(text_line) + Decoder.last_digit(text_line)
+        decoded = Decoder._first_digit(text_line) + Decoder._last_digit(text_line)
+        return int(decoded)
+    
+    def decode_with_spelling(text_line):
+        decoded = Decoder._first_number(text_line) + Decoder._last_number(text_line)
         return int(decoded)
 
-    def first_digit(text_line):
-        i = 0
-        while i < len(text_line):
-            if text_line[i].isdigit():
-                return text_line[i]
-            i += 1
+    def _first_number(text_line):
+        converted = Decoder._convert_first_digit(text_line)
+        return Decoder._first_digit(converted)
+    
+    def _last_number(text_line):
+        converted = Decoder._convert_last_digit(text_line)
+        return Decoder._last_digit(converted)
 
-    def last_digit(text_line):
-        i = len(text_line) - 1
-        while i >= 0:
-            if text_line[i].isdigit():
-                return text_line[i]
-            i -= 1
+    def _convert_first_digit(text):
+        regex_pattern = '|'.join(re.escape(key) for key in translator.keys())
+        return re.sub(regex_pattern, lambda m: translator[m.group(0)], text, count=1)
+    
+    def _convert_last_digit(text):
+        text_reversed = Decoder._reverse_string(text)        
+        translator_reversed = Decoder._reverse_dict(translator)
+        
+        regex_pattern = '|'.join(re.escape(key) for key in translator_reversed.keys())
+        result_reversed = re.sub(regex_pattern, lambda m: translator_reversed[m.group(0)], text_reversed, count=1)
+        result = Decoder._reverse_string(result_reversed)        
+        return result
+
+    def _reverse_string(s):
+        return s[::-1]
+
+    def _reverse_dict(d):
+        return {k[::-1]: v for k, v in d.items()}
 
     def sum_decoded_from_file(file):
         f = open(file, 'r')
@@ -25,3 +55,27 @@ class Decoder:
         for line in lines:
             sum += Decoder.decode(line)
         return sum
+
+    def sum_decoded_with_spelling_from_file(file):
+        f = open(file, 'r')
+        lines = f.readlines()
+        f.close()
+        sum = 0
+        for line in lines:
+            sum += Decoder.decode_with_spelling(line)
+        return sum
+
+    def _first_digit(text_line):
+        i = 0
+        while i < len(text_line):
+            if text_line[i].isdigit():
+                return text_line[i]
+            i += 1
+
+    def _last_digit(text_line):
+        i = len(text_line) - 1
+        while i >= 0:
+            if text_line[i].isdigit():
+                return text_line[i]
+            i -= 1
+
